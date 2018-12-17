895bc29b613741735dfd8a1571e2a985348e3e7b
Agustín Quetto <agustinquetto@gmail.com>
2018-12-17 04:19:32 -0300

    try except

--- PagoFacil-v2.py
+++ PagoFacil-v2.py
@@ -4,38 +4,52 @@ from utils import *
 
 #solicita las provincias a rapipago y retorna una lista con todas
 def getProvinces():
-    res = requests.get('https://www.e-pagofacil.com/', verify=False)
-    soup = BeautifulSoup(res.text, 'lxml')
-    provincesExtract = soup.find('select', {'name': 'Provincia'}).findChildren('option')
-    provincesExtract.pop(0)
-    extracted = []
-    for item in provincesExtract:
-        province = [findBetweenR(str(item), '<option value="', '">'), item.get_text()]
-        extracted.append(province)
-    return extracted
+    try:
+        res = requests.get('https://www.e-pagofacil.com/', verify=False)
+        if res.status_code != 200:
+            raise ValueError('Error solicitando provincias.')
+        soup = BeautifulSoup(res.text, 'lxml')
+        provincesExtract = soup.find('select', {'name': 'Provincia'}).findChildren('option')
+        provincesExtract.pop(0)
+        extracted = []
+        for item in provincesExtract:
+            province = [findBetweenR(str(item), '<option value="', '">'), item.get_text()]
+            extracted.append(province)
+        return extracted
+    except Exception as e:
+        print(e)
 
 #solicita las localidades mediante el id de la provincia y retorna una lista
 def getLocalidades(provinceId):
-    body = {'idprov': provinceId}
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-Requested-With': 'XMLHttpRequest'}
-    res = requests.post('https://www.e-pagofacil.com/clientes/contacto', verify=False, headers=headers, data=body)
-    res.encoding = 'utf-8'
-    dicted = []
-    if res.status_code == 200:
-        for item in json.loads(res.text):
-            dicted.append([item['id_loca'], item['de_loca']])
-        return dicted
-    return False
+    try:
+        body = {'idprov': provinceId}
+        headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-Requested-With': 'XMLHttpRequest'}
+        res = requests.post('https://www.e-pagofacil.com/clientes/contacto', verify=False, headers=headers, data=body)
+        if res.status_code != 200:
+            raise ValueError('Error solicitando localidades.')
+        res.encoding = 'utf-8'
+        dicted = []
+        if res.status_code == 200:
+            for item in json.loads(res.text):
+                dicted.append([item['id_loca'], item['de_loca']])
+            return dicted
+        return False
+    except Exception as e:
+        print(e)
 
 #solicita las localidades mediante el id de la provincia y retorna una lista
 def getLocation(provinceId, locationId):
-    res = requests.get('https://www.e-pagofacil.com/backend/intranet/coordenadas.php?p='+provinceId+'&l='+locationId, verify=False)
-    dicted = []
-    if res.status_code == 200:
+    try:
+        res = requests.get('https://www.e-pagofacil.com/backend/intranet/coordenadas.php?p='+provinceId+'&l='+locationId, verify=False)
+        dicted = []
+        if res.status_code != 200:
+            raise ValueError('Error solicitando localizacion e informacion.')
         for item in json.loads(res.text):
             dicted.append(list(item.values()))
         print(dicted)
         return dicted
+    except Exception as e:
+        print(e)
 
 #funcion que se llama y contiene la ejecucion del programa
 def run():
@@ -48,10 +62,13 @@ def run():
     for d in paths.keys():
         dirs(paths[d])
     provinces = getProvinces()
+    if len(provinces) < 1:
+        raise ValueError('No se obtuvieron provincias.')
     toCSV(paths['provincias']+'/CodigoProvincias.csv', provinces, ['ID', 'PROVINCIA'])
     for p in provinces:
         localidades = getLocalidades(p[0])
-        print(localidades)
+        if len(localidades) < 1:
+            raise ValueError('No se obtuvieron localidades.')
         toCSV(paths['localidades']+'/Localidad-'+p[1]+'.csv', localidades, ['ID', 'ID-PROVINCIA', 'LOCALIDAD'])
         for l in localidades:
             location = getLocation(p[0], l[1])
@@ -60,4 +77,7 @@ def run():
                     'WESTERN_UNION','ENVIO_NACIONAL','PAGO_FACTURAS','RECARGA_FACIL',
                     'HS_LAV','HS_SABADO','HS_DOMINGO','LATITUD','LONGITUD'])
 
-run()
+try:
+    run()
+except Exception as e:
+    print(e)
--- RapiPago-v2.py
+++ RapiPago-v2.py
@@ -5,36 +5,51 @@ from utils import *
 
 #solicita las provincias a rapipago y retorna una lista con todas
 def getProvinces():
-    res = requests.get('https://www.rapipago.com.ar/rapipagoWeb/index.php')
-    soup = BeautifulSoup(res.text, 'lxml')
-    provincesExtract = soup.select('#field-provincia > option')
-    provincesExtract.pop(0)
-    extracted = []
-    for item in provincesExtract:
-        province = [findBetweenR(str(item), '<option value="', '">'), item.get_text()]
-        extracted.append(province)
-    return extracted
+    try:
+        res = requests.get('https://www.rapipago.com.ar/rapipagoWeb/index.php')
+        if res.status_code != 200:
+            raise ValueError('Error solicitando provincias.')
+        soup = BeautifulSoup(res.text, 'lxml')
+        provincesExtract = soup.select('#field-provincia > option')
+        provincesExtract.pop(0)
+        extracted = []
+        for item in provincesExtract:
+            province = [findBetweenR(str(item), '<option value="', '">'), item.get_text()]
+            extracted.append(province)
+        return extracted
+    except Exception as e:
+        print(e)
 
 #solicita las localidades mediante el id de la provincia y retorna una lista
 def getLocalidades(provinceId):
-    res = requests.get('https://www.rapipago.com.ar/rapipagoWeb/index.php/services/localidades/'+provinceId)
-    localidades = json.loads(res.text)['data']
-    clean = []
-    for loc in localidades:
-        clean.append([loc['id_prov_partido_localidad'], provinceId, loc['descripcion']])
-    return clean
+    try:
+        res = requests.get('https://www.rapipago.com.ar/rapipagoWeb/index.php/services/localidades/'+provinceId)
+        if res.status_code != 200:
+            raise ValueError('Error solicitando localidades.')
+        localidades = json.loads(res.text)['data']
+        clean = []
+        for loc in localidades:
+            clean.append([loc['id_prov_partido_localidad'], provinceId, loc['descripcion']])
+        return clean
+    except Exception as e:
+        print(e)
 
 #solicita las sucursales mediante el id de provincia y localidad, extrayendo cada una del contenido del sitio
 #retorna una lista
 def getBranches(provinceId, localidadId, extracash=False):
-    extracash = 'true' if extracash else 'false'
-    res = requests.get('https://www.rapipago.com.ar/rapipagoWeb/index.php/resultado_sucursales?prov='+provinceId+'&loc='+localidadId+'&extracash='+extracash) 
-    soup = BeautifulSoup(res.text, 'lxml')
-    tableContent = soup.select('.tableizer-table')
-    extractor = Extractor(str(tableContent))
-    extractor.parse()
-    listed = extractor.return_list()
-    return listed
+    try:
+        extracash = 'true' if extracash else 'false'
+        res = requests.get('https://www.rapipago.com.ar/rapipagoWeb/index.php/resultado_sucursales?prov='+provinceId+'&loc='+localidadId+'&extracash='+extracash)
+        if res.status_code != 200:
+            raise ValueError('Error solicitando sucursales.') 
+        soup = BeautifulSoup(res.text, 'lxml')
+        tableContent = soup.select('.tableizer-table')
+        extractor = Extractor(str(tableContent))
+        extractor.parse()
+        listed = extractor.return_list()
+        return listed
+    except Exception as e:
+        print(e)
 
 #retorna una nueva lista con los datos finales necesarios de las sucursales extraidas en bruto
 def necessaryBranches(branches):
@@ -45,25 +60,38 @@ def necessaryBranches(branches):
 
 #funcion que se llama y contiene la ejecucion del programa
 def run():
-    paths = {
-                'localidades': 'localidades_Rapipago',
-                'provincias': 'provincias_Rapipago',
-                'sucursales': 'ArchivosCSV_Rapipago'
-            }
+    try:
+        paths = {
+                    'localidades': 'localidades_Rapipago',
+                    'provincias': 'provincias_Rapipago',
+                    'sucursales': 'ArchivosCSV_Rapipago'
+                }
 
-    for d in paths.keys():
-        dirs(paths[d])
-    provinces = getProvinces()
-    toCSV(paths['provincias']+'/CodigoProvincias.csv', provinces, ['ID', 'PROVINCIA'])
-    for p in provinces:
-        localidades = getLocalidades(p[0])
-        toCSV(paths['localidades']+'/Localidad-'+p[1]+'.csv', localidades, ['ID', 'ID-PROVINCIA', 'LOCALIDAD'])
-        for loc in localidades:
-            branches = getBranches(p[0], loc[0])
-            #header = branches[0]
-            branches.pop(0)
-            necessary = necessaryBranches(branches)
-            toCSV(paths['sucursales']+'/Sucursales_'+p[1]+'_'+loc[2]+'_RapiPago.csv',
-                    necessary, ['Localidad','Sucursal','Direccion'])
+        for d in paths.keys():
+            dirs(paths[d])
+        provinces = getProvinces()
+        if len(provinces) < 1:
+            raise ValueError('No se obtuvieron provincias.')
+        toCSV(paths['provincias']+'/CodigoProvincias.csv', provinces, ['ID', 'PROVINCIA'])
 
-run()
+        for p in provinces:
+            localidades = getLocalidades(p[0])
+            if len(localidades) < 1:
+                raise ValueError('No se obtuvieron localidades.')
+            toCSV(paths['localidades']+'/Localidad-'+p[1]+'.csv', localidades, ['ID', 'ID-PROVINCIA', 'LOCALIDAD'])
+
+            for loc in localidades:
+                branches = getBranches(p[0], loc[0])
+                #header = branches[0]
+                branches.pop(0)
+                necessary = necessaryBranches(branches)
+                toCSV(paths['sucursales']+'/Sucursales_'+p[1]+'_'+loc[2]+'_RapiPago.csv',
+                        necessary, ['Localidad','Sucursal','Direccion'])
+
+    except Exception as e:
+        print(e)
+
+try:
+    run()
+except Exception as e:
+    print(e)
